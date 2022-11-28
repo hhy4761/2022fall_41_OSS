ee3884196bba1edf30f1af175abbdd18e5c81e78
우지은 <>
2022-11-29 01:33:42 +0900

    href ejs download error fix

--- router.js
+++ router.js
@@ -19,13 +19,41 @@ router.get('/login', (req,res) => {
 router.get('/survey',(req,res) => {
     res.render('survey');
 })
+router.get('/surveyresult/:result',(req,res) => {
+    switch (req.params.result) {
+        case 'human':
+            res.render('surveyresult/human')
+            break
+        case 'engineer':
+            res.render('surveyresult/engineer')
+            break
+        case 'design':
+            res.render('surveyresult/design')
+            break
+        case 'manage':
+            res.render('surveyresult/manage')
+            break
+        case 'exercise':
+            res.render('surveyresult/exercise')
+            break
+        default:
+            res.render('survey');
+            break
+    }
+})
 router.get('/main',(req,res) => {
     res.render('main');
 })
 router.get('/board',(req,res) => {
     fetch('http://127.0.0.1:4500/apis/listBoard')
     .then(response => response.json())
-    .then(response => res.render('board.ejs', {posts: response}))
+    .then(response => res.render('board', {posts: response}))
+})
+router.get('/board/:id', (req, res) => {
+    fetch(`http://127.0.0.1:4500/apis/getBoard/${req.params.id}`)
+    .then(response => response.json())
+    .then(response => {res.render('boardPost', {post: response.data})
+console.log(response)})
 })
 
 /*
